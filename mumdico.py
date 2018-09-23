ba18314fc84be8e2caee215835bb4d281536765c
Raphaël Charrat <mrraph_@MacBouc.lan>
2018-09-23 18:33:59 +0200

    Added reaction.

--- mumdico.py
+++ mumdico.py
@@ -8,15 +8,19 @@ import re
 @listen_to('(.*)gratt(.*)', re.IGNORECASE)
 def gratter(message, before='', after=''):
     message.reply(format(before) + 'trait' + format(after), in_thread=True)
+    message.react('face_with_monocle')
 
 @listen_to('(.*)trait(.*)', re.IGNORECASE)
 def traiter(message, before='', after=''):
     message.reply(format(before) + 'gratt' + format(after), in_thread=True)
+    message.react('face_with_monocle')
 
 @listen_to('(.*)copine(.*)', re.IGNORECASE)
 def Villefranche(message, before='', after=''):
     message.reply(format(before) + 'Villefranche' + format(after), in_thread=True)
+    message.react('face_with_monocle')
 
 @listen_to('(.*)villefranche(.*)', re.IGNORECASE)
 def copine(message, before='', after=''):
     message.reply(format(before) + 'copine' + format(after), in_thread=True)
+    message.react('face_with_monocle')
